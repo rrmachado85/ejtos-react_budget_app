219940a698dc46bfa394c3a6b3c5f8baa1dd1317
Rui Machado <rui.rodrigues-machado@gmail.com>
2022-12-13 13:39:38 -0500

    Task1 and Task2 done

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,11 +2,25 @@ import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget,dispatch } = useContext(AppContext);
+
+    const changeBudget = (budget) => {
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: budget
+        });
+    };
 
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget: £</span>
+            <input
+                required='required'
+                type='number'
+                id='budget'
+                value={budget}
+                onChange={event=> changeBudget(event.target.value)}>
+            </input>
         </div>
     );
 };
